621f1c928e6222bd199bea8d20bdf547e87d9df9
Paul Kibet <nmurgor10@gmail.com>
2022-09-08 16:05:47 +0300

    Add static types to `modules`

---
+++ service/src/modules/index.ts
@@ -0,0 +1,10 @@
+import eth from "./eth";
+import bsc from "./bsc";
+
+export * as RPC from "./rpc";
+export * as Db from "./db";
+export * as Data from "./data";
+export * as Chores from "./chores";
+
+export const Eth = eth;
+export const Bsc = bsc;
