f1d76b3744da7fb7b1b400957274b34ebce8adf3
0xSwakei <kewebmasters@gmail.com>
2022-10-23 10:41:18 +0300

    update `tests/Header.test.js` to work with ( #14 )

--- app/src/tests/components/Header.test.js
+++ app/src/tests/components/Header.test.js
@@ -1,10 +1,16 @@
-import {render, screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 import Header from '../../components/Header'
+import { MetaMaskProvider } from 'metamask-react'
 
 test('renders Header component', () => {
-  render(<Header/>)
+  render(
+    <MetaMaskProvider>
+      <Header />
+    </MetaMaskProvider>
+  )
+  
   const linkText = screen.getByText('Bitgesell-WBGL Bridge')
   expect(linkText).toBeInTheDocument()
 })
